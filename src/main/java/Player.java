1f5e7c567b1064135d09de0fd94f9ed7e8523bb4
melscottjr77 <melscottjr77@gmail.com>
2023-03-02 03:57:40 +0000

    Complete Lab Constructors

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -16,7 +16,6 @@
 public class Player {
     public String playerName;
     public int level;
-
     /**
      * this.playerName should be set to playerName.
      *
@@ -31,10 +30,9 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-        this.playerName = "Makro";
+        this.playerName = playerName;
         this.level = 1;
     }
-
     /**
      * Again, the values of this object should be set with the help of the 'this' keyword to avoid confusion between
      * class fields / parameter variables.
@@ -49,7 +47,7 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
-        this.playerName = "Vikas";
+        this.playerName = playerName;
         this.level = 5;
     }
 
